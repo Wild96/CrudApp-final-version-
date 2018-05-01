83507dfc4f7795c735544c3d44afc583b35397a2
sai prasath <saiprasath801@gmail.com>
2018-05-01 13:30:56 +0530

    Changed app.js

---
+++ app.js
@@ -0,0 +1,104 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/node-auth')
+  .then(() =>  console.log('succesfully connected to node auth database'))
+  .catch((err) => console.error(err));
+
+//var parseCookie = express.cookieParser(secret);
+//var store = /* your MemoryStore, RedisStore, etc */;
+
+var index = require('./routes/index');
+var users = require('./routes/users');
+var employees = require('./routes/employees');
+//adding mongostore
+var MongoStore = require('connect-mongo')(session);
+var app = express();
+//Mongoose model linkage 
+var chat = require('./models/chat');
+var session = require("express-session")({
+  secret: "my-secret",
+  resave: true,
+  saveUninitialized: true,
+  store: new MongoStore({
+    mongooseConnection: mongoose.connection,
+    ttl: 2 * 24 * 60 * 60
+  })
+}),
+  sharedsession = require("express-socket.io-session");
+//socket code
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+var sess;//added;
+
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+//sending socket io response as to middleware
+app.use(function (req, res, next) {
+  res.io = io;
+  next();
+});
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(session);
+io.use(sharedsession(session));
+  /*({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false,
+  store: new MongoStore({
+    mongooseConnection: mongoose.connection,
+    ttl: 2 * 24 * 60 * 60
+  })
+}));*/
+///session code//
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', index);
+app.use('/users', users);
+app.use('/employees', employees);
+
+// passport configuration
+var User = require('./models/User');
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+//module.exports = app;
+//socket code
+module.exports = { app: app, server: server };
